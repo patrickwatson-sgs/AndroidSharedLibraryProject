ee80e2a3e16e7d523b3b8b844068656b56c64e7e
Patrick Watson <patrick.watson@sato-global.com>
2017-05-30 16:07:18 -0400

    Added shared string

---
+++ AndroidSharedLibraryModule/src/main/java/com/satoglobalsolutions/patrickwatson/androidsharedlibraryproject/SharedClass.java
@@ -0,0 +1,10 @@
+package com.satoglobalsolutions.patrickwatson.androidsharedlibraryproject;
+
+import android.content.Context;
+
+public class SharedClass {
+
+    public String sharedString(){
+        return "test";
+    }
+}
